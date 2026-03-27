0d834e8b0bbd6686bc71e6172a42e0302263f180
ValenDuarte1918 <valentinduarte1@hotmail.com>
2026-03-26 21:31:55 -0300

    Redireccion Login - RegistrarCuenta
    Se vinculo el btn de Crear Cuenta para que direccione a la vista de Registrar Cuenta

--- app/src/main/java/FollowUp/Login.kt
+++ app/src/main/java/FollowUp/Login.kt
@@ -1,6 +1,8 @@
 package FollowUp
 
+import android.content.Intent
 import android.os.Bundle
+import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -12,10 +14,18 @@ class Login : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_login)
+        
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+
+        // Configurar el clic para ir a la pantalla de Registro
+        val tvRegister = findViewById<TextView>(R.id.tv_Register)
+        tvRegister.setOnClickListener {
+            val intent = Intent(this, RegistrarCuenta::class.java)
+            startActivity(intent)
         }
     }
 }
