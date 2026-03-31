7f856e549b2acc07cea0fc2c925f7651d24eb5da
LuccaPerini <luccaxxperini@gmail.com>
2026-03-31 01:47:16 -0300

    PANTALLAS: Codigo y Nueva Contraseña

---
+++ app/src/main/java/com/followup/fragments/ReestablecerFragment.kt
@@ -0,0 +1,21 @@
+package com.followup.fragments
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.followup.R
+
+class ReestablecerFragment : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_reestablecer_fragment)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}
