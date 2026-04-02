f58d2d0fec06b407fc0be22b133e2de5b1f52978
ValenDuarte1918 <valentinduarte1@hotmail.com>
2026-04-02 10:37:38 -0300

    Implemente saludo personalizado en InicioFragment y mejore el diseño de la interfaz

--- app/src/main/java/com/followup/fragments/InicioFragment.kt
+++ app/src/main/java/com/followup/fragments/InicioFragment.kt
@@ -1,20 +1,31 @@
 package com.followup.fragments
 
+import android.content.Context
+import android.content.SharedPreferences
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 import com.followup.R
 
+// sharedPreferences para mostrar el nombre del usuario en el saludo, es una añternativa que encontre a ViewModel.
+// Seria como un localStorage de Android
 class InicioFragment : Fragment() {
+    private lateinit var sharedPreferences: SharedPreferences
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_inicio, container, false)
-    }
+        val view = inflater.inflate(R.layout.fragment_inicio, container, false)
+        sharedPreferences = requireActivity().getSharedPreferences("FollowUp_prefs", Context.MODE_PRIVATE)
 
+        val userName = sharedPreferences.getString("USER_NAME", "Usuario")
+        val saludoTextView = view.findViewById<TextView>(R.id.tv_saludo)
+        saludoTextView.text = "Hola, $userName"
+
+        return view
+    }
 }
